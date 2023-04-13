e4cacfa4ee14472c6041cfe15cdabb2ec4d5796e
Yoel Regalado <yoelregalado7@gmail.com>
2023-04-13 15:55:39 -0600

    adjusted handlebars

--- public/js/comment.js
+++ public/js/comment.js
@@ -4,17 +4,35 @@ const commentHandler = async (event) => {
   const title = document.querySelector('#post-name').value.trim();
   const description = document.querySelector('#post-desc').value.trim();
 
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/post/${id}`, {
-      method: 'COMMENT',
+   if (title && description) {
+    const response = await fetch(`/api/comment`, {
+      method: 'POST',
+      body: JSON.stringify({ title, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
 
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to comment on post');
+      alert('Failed to create comment');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/post/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete post');
     }
   }
 };
